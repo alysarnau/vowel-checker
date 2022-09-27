98d330359aeb67e5e310cb86cd97fd1a691c7b2c
alysvolatile <alice.g.cooper@gmail.com>
2022-09-27 10:45:14 -0400

    final styling

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let wordToCheck = document.getElementById("word").value
-let checkButton = document.getElementById("submitBtn")
+let checkButton = document.getElementById("checkButton")
 let displayDiv = document.getElementById('results')
 
 function vowelCounter() {
     strToCheck = document.getElementById("word").value
+    if (strToCheck < 1) {
+        alert(`Please enter a word to test.`)
+        return
+    }
     let counter = 0;
-    // create regex to check against
     let vowels = new RegExp(/[aeiou]/)
-    // go through each char in string
     for (let char in strToCheck) {
-        // if vowel, add to counter
         if (vowels.test(strToCheck.charAt(char))) {
             ++counter;
         }
     }
     console.log('total vowels: ', counter)    
-    displayDiv.innerText = `There are ${counter} total vowels in "${strToCheck}".`   
+    if (counter > 1) {
+        displayDiv.innerText = `There are ${counter} total vowels in "${strToCheck}".`   
+    } else {
+        displayDiv.innerText = `There is ${counter} total vowel in "${strToCheck}".`   
+    }
 }
 
 checkButton.addEventListener('click', vowelCounter)
